3de4feab5362c4d1a1eca05f655323301eccad69
Chizzy <emailMe@chizzys.com>
2014-07-04 15:02:44 -0700

    init

--- lib/hermit/workers/config.rb
+++ lib/hermit/workers/config.rb
@@ -10,7 +10,6 @@ Sidekiq.configure_server do |config|
   config.options[:queues] = ["google_search", "bing_search", "outbound_data"]
   config.options[:concurrency] = 25
   config.options[:verbose] = true
-  config.options[:pidfile] = "tmp/pids/sidekiq.pid"
 end
 
 Sidekiq::Queue['google_search'].limit = 1
